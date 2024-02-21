46f7cfc41608aa22f73b9edfa55a91f46837df30
Kirill Egorov <kirill.egorov@x5.ru>
2024-02-21 19:47:12 +0200

    .env

--- src/api/index.js
+++ src/api/index.js
@@ -1,22 +1,22 @@
-import axios from 'axios'
-import fixTime from '@/utils/fixTime'
+import axios from "axios";
+import fixTime from "@/utils/fixTime";
 
-const API_KEY = '7efa332cf48aeb9d2d391a51027f1a71'
-const API_URL_ONECALL = 'https://api.openweathermap.org/data/2.5/onecall'
-const API_URL_WEATHER = 'https://api.openweathermap.org/data/2.5/weather'
+const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+const API_URL_ONECALL = "https://api.openweathermap.org/data/2.5/onecall";
+const API_URL_WEATHER = "https://api.openweathermap.org/data/2.5/weather";
 
 async function oneCall(query) {
   const resp = await axios.get(
     `${API_URL_ONECALL}?lat=${query.lat}&lon=${query.lng}&exclude=minutely&appid=${API_KEY}&units=metric`
-  )
-  return fixTime(resp)
+  );
+  return fixTime(resp);
 }
 
 async function weather(query) {
   const resp = await axios.get(
     `${API_URL_WEATHER}?lat=${query.lat}&lon=${query.lng}&appid=${API_KEY}&units=metric`
-  )
-  return resp
+  );
+  return resp;
 }
 
-export default { oneCall, weather }
+export default { oneCall, weather };
